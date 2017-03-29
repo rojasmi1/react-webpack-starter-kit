285a10c2c6ed48dd0b01e2bdd008d6d781547244
Michael <michael.rojasp@gmail.com>
2017-03-28 21:53:22 -0600

    Add Boostrap

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
+     <div className="jumbotron">
         <h1>Hello World</h1>
       </div>);
   }
