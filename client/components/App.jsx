91bf29292428adb9de9c35a8c1c3b68abca15582
Michael Rojas <michael.rojasp@gmail.com>
2017-03-29 10:43:56 -0600

    Import core.css as module inside App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import classes from '../css/core.css';
 
 export default class App extends React.Component {
   render() {
     return (
      <div className="jumbotron">
-        <h1>Hello World</h1>
-      </div>);
+        <h1>Minimalistic React Starter Kit</h1>
+        <div><a className={classes.link} target="_blank" href="https://github.com/rojasmi1/react-webpack-starter-kit">Fork me in github</a></div>
+     </div>
+    );
   }
 }
